1648984ce34469513be8fb9c0c19194e3b3f7e0d
Sanjeet Pathak <sanjeet.pathak990@gmail.com>
2019-07-23 21:02:03 +0530

    Changes

--- appServer.js
+++ appServer.js
@@ -452,6 +452,8 @@ app.post('/reorder', async function (req, res) {
 			var order = req.body.order;
 			var promiseMongo = initDBConnection();
 
+			console.log(order);
+
 			promiseMongo.then(function(dbo){
 				dbo.collection("users").find({"devices":{$all :[deviceId]}}).toArray(function(err, result) {
 					if(err){
